512bf9ef0c795f2bf178ed64cdccb1f5d04edb04
nook4124129 <nookandnui@hotmail.com>
2015-04-27 00:45:28 +0700

    change first prize program1 no.

--- program1.c
+++ program1.c
@@ -4,12 +4,12 @@ int main()
 {
    int a, b, c;
 
-   printf("Enter two numbers to add\n");
+   printf("Enter two numbers to minus\n");
    scanf("%d%d",&a,&b);
 
    c = a - b;
 
-   printf("Sub of entered numbers = %d\n",c);
+   printf("Sub of input numbers = %d\n",c);
 
    return 0;
 }
--- program2.c
+++ program2.c
@@ -7,10 +7,10 @@ int main()
    printf("Enter an integer\n");
    scanf("%d", &n);
 
-   if (n%2 == 0)
-      printf("Even\n");
-   else
+   if (n%2 != 0)
       printf("Odd\n");
+   else
+      printf("Even\n");
 
    return 0;
 }
